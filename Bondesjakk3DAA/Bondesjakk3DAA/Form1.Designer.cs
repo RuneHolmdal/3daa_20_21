b97ae0953597a023bcdac13bb91927b465e187b8
Rune Holmdal <runehol@viken.no>
2020-09-24 13:35:20 +0200

    Ferdig bondesjakk
    Slik det ble etter timen den 24.09

---
+++ Bondesjakk3DAA/Bondesjakk3DAA/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Bondesjakk3DAA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b11 = new System.Windows.Forms.Button();
+            this.b12 = new System.Windows.Forms.Button();
+            this.b13 = new System.Windows.Forms.Button();
+            this.b23 = new System.Windows.Forms.Button();
+            this.b22 = new System.Windows.Forms.Button();
+            this.b21 = new System.Windows.Forms.Button();
+            this.b33 = new System.Windows.Forms.Button();
+            this.b32 = new System.Windows.Forms.Button();
+            this.b31 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bNyttSpill = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // b11
+            // 
+            this.b11.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b11.Location = new System.Drawing.Point(110, 329);
+            this.b11.Name = "b11";
+            this.b11.Size = new System.Drawing.Size(100, 100);
+            this.b11.TabIndex = 0;
+            this.b11.UseVisualStyleBackColor = true;
+            this.b11.Click += new System.EventHandler(this.Spiller_Click);
+            // 
+            // b12
+            // 
+            this.b12.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b12.Location = new System.Drawing.Point(216, 329);
+            this.b12.Name = "b12";
+            this.b12.Size = new System.Drawing.Size(100, 100);
+            this.b12.TabIndex = 1;
+            this.b12.UseVisualStyleBackColor = true;
+            this.b12.Click += new System.EventHandler(this.Spiller_Click);
+            // 
+            // b13
+            // 
+            this.b13.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b13.Location = new System.Drawing.Point(322, 329);
+            this.b13.Name = "b13";
+            this.b13.Size = new System.Drawing.Size(100, 100);
+            this.b13.TabIndex = 2;
+            this.b13.UseVisualStyleBackColor = true;
+            this.b13.Click += new System.EventHandler(this.Spiller_Click);
+            // 
+            // b23
+            // 
+            this.b23.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b23.Location = new System.Drawing.Point(322, 223);
+            this.b23.Name = "b23";
+            this.b23.Size = new System.Drawing.Size(100, 100);
+            this.b23.TabIndex = 5;
+            this.b23.UseVisualStyleBackColor = true;
+            this.b23.Click += new System.EventHandler(this.Spiller_Click);
+            // 
+            // b22
+            // 
+            this.b22.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b22.Location = new System.Drawing.Point(216, 223);
+            this.b22.Name = "b22";
+            this.b22.Size = new System.Drawing.Size(100, 100);
+            this.b22.TabIndex = 4;
+            this.b22.UseVisualStyleBackColor = true;
+            this.b22.Click += new System.EventHandler(this.Spiller_Click);
+            // 
+            // b21
+            // 
+            this.b21.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b21.Location = new System.Drawing.Point(110, 223);
+            this.b21.Name = "b21";
+            this.b21.Size = new System.Drawing.Size(100, 100);
+            this.b21.TabIndex = 3;
+            this.b21.UseVisualStyleBackColor = true;
+            this.b21.Click += new System.EventHandler(this.Spiller_Click);
+            // 
+            // b33
+            // 
+            this.b33.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b33.Location = new System.Drawing.Point(322, 117);
+            this.b33.Name = "b33";
+            this.b33.Size = new System.Drawing.Size(100, 100);
+            this.b33.TabIndex = 8;
+            this.b33.UseVisualStyleBackColor = true;
+            this.b33.Click += new System.EventHandler(this.Spiller_Click);
+            // 
+            // b32
+            // 
+            this.b32.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b32.Location = new System.Drawing.Point(216, 117);
+            this.b32.Name = "b32";
+            this.b32.Size = new System.Drawing.Size(100, 100);
+            this.b32.TabIndex = 7;
+            this.b32.UseVisualStyleBackColor = true;
+            this.b32.Click += new System.EventHandler(this.Spiller_Click);
+            // 
+            // b31
+            // 
+            this.b31.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b31.Location = new System.Drawing.Point(110, 117);
+            this.b31.Name = "b31";
+            this.b31.Size = new System.Drawing.Size(100, 100);
+            this.b31.TabIndex = 6;
+            this.b31.UseVisualStyleBackColor = true;
+            this.b31.Click += new System.EventHandler(this.Spiller_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 9;
+            // 
+            // bNyttSpill
+            // 
+            this.bNyttSpill.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bNyttSpill.Location = new System.Drawing.Point(110, 450);
+            this.bNyttSpill.Name = "bNyttSpill";
+            this.bNyttSpill.Size = new System.Drawing.Size(312, 100);
+            this.bNyttSpill.TabIndex = 10;
+            this.bNyttSpill.Text = "Nytt spill";
+            this.bNyttSpill.UseVisualStyleBackColor = true;
+            this.bNyttSpill.Click += new System.EventHandler(this.bNyttSpill_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(247, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 562);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.bNyttSpill);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b33);
+            this.Controls.Add(this.b32);
+            this.Controls.Add(this.b31);
+            this.Controls.Add(this.b23);
+            this.Controls.Add(this.b22);
+            this.Controls.Add(this.b21);
+            this.Controls.Add(this.b13);
+            this.Controls.Add(this.b12);
+            this.Controls.Add(this.b11);
+            this.Name = "Form1";
+            this.Text = "Bondesjakk";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b11;
+        private System.Windows.Forms.Button b12;
+        private System.Windows.Forms.Button b13;
+        private System.Windows.Forms.Button b23;
+        private System.Windows.Forms.Button b22;
+        private System.Windows.Forms.Button b21;
+        private System.Windows.Forms.Button b33;
+        private System.Windows.Forms.Button b32;
+        private System.Windows.Forms.Button b31;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bNyttSpill;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Bondesjakk3DAA/Bondesjakk3DAA/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bondesjakk3DAA
+{
+    public partial class Form1 : Form{
+        public Form1(){
+            InitializeComponent();
+        }
+
+        bool spiller1 = true;
+        const int maksAntallTrekk = 9;
+        int antallTrekk = 0;
+
+        private bool Vinner(string s){
+            bool v = (b11.Text == s && b12.Text == s && b13.Text == s) ||
+                    (b21.Text == s && b22.Text == s && b23.Text == s) ||
+                    (b31.Text == s && b32.Text == s && b33.Text == s) ||
+                    (b11.Text == s && b21.Text == s && b31.Text == s) ||
+                    (b12.Text == s && b22.Text == s && b32.Text == s) ||
+                    (b13.Text == s && b23.Text == s && b33.Text == s) ||
+                    (b11.Text == s && b22.Text == s && b33.Text == s) ||
+                    (b13.Text == s && b22.Text == s && b31.Text == s);
+            return v;
+        }
+        private void SjekkVinner(){
+            if (Vinner("X")){
+                label1.Text = "X er vinner";
+                KontrollerSpillflate(false);
+            }
+            else if (Vinner("O")){
+                label1.Text = "O er vinner";
+                KontrollerSpillflate(false);
+            }
+            else if (antallTrekk == maksAntallTrekk)
+                label1.Text = "Uavgjordt!";
+        }
+
+        private void KontrollerSpillflate(bool k){
+            b11.Enabled = k;
+            b12.Enabled = k;
+            b13.Enabled = k;
+            b21.Enabled = k;
+            b22.Enabled = k;
+            b23.Enabled = k;
+            b31.Enabled = k;
+            b32.Enabled = k;
+            b33.Enabled = k;
+        }
+        private void NyttSpill(){
+            b11.Text = "";
+            b12.Text = "";
+            b13.Text = "";
+            b21.Text = "";
+            b22.Text = "";
+            b23.Text = "";
+            b31.Text = "";
+            b32.Text = "";
+            b33.Text = "";
+            antallTrekk = 0;
+            KontrollerSpillflate(true);
+        }
+
+        private void bNyttSpill_Click(object sender, EventArgs e){
+            NyttSpill();
+        }
+
+        private void Spiller_Click(object sender, EventArgs e){
+            Button b = sender as Button;
+
+            if (b.Text != "X" && b.Text != "O"){
+                if (spiller1)
+                    b.Text = "X";
+                else
+                    b.Text = "O";
+                spiller1 = !spiller1;
+                antallTrekk = antallTrekk + 1;
+                SjekkVinner();
+            }
+        }
+    }
+}
