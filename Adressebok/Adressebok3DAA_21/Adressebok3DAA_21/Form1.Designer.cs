df016e97617d8986eca1001f81ab0e42f62bbfee
Rune Holmdal <runehol@viken.no>
2021-02-11 14:03:31 +0100

    Adressebok: Ajour med undervisning 11.02

---
+++ Adressebok/Adressebok3DAA_21/Adressebok3DAA_21/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Adressebok3DAA_21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btLagre = new System.Windows.Forms.Button();
+            this.tbNavn = new System.Windows.Forms.TextBox();
+            this.tbTlf = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btLagre
+            // 
+            this.btLagre.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLagre.Location = new System.Drawing.Point(198, 234);
+            this.btLagre.Name = "btLagre";
+            this.btLagre.Size = new System.Drawing.Size(126, 61);
+            this.btLagre.TabIndex = 0;
+            this.btLagre.Text = "Lagre";
+            this.btLagre.UseVisualStyleBackColor = true;
+            this.btLagre.Click += new System.EventHandler(this.btLagre_Click);
+            // 
+            // tbNavn
+            // 
+            this.tbNavn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNavn.Location = new System.Drawing.Point(198, 95);
+            this.tbNavn.Name = "tbNavn";
+            this.tbNavn.Size = new System.Drawing.Size(126, 44);
+            this.tbNavn.TabIndex = 1;
+            // 
+            // tbTlf
+            // 
+            this.tbTlf.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTlf.Location = new System.Drawing.Point(198, 163);
+            this.tbTlf.Name = "tbTlf";
+            this.tbTlf.Size = new System.Drawing.Size(126, 44);
+            this.tbTlf.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(90, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Navn:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(90, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 37);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tlf.:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbTlf);
+            this.Controls.Add(this.tbNavn);
+            this.Controls.Add(this.btLagre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btLagre;
+        private System.Windows.Forms.TextBox tbNavn;
+        private System.Windows.Forms.TextBox tbTlf;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Adressebok/Adressebok3DAA_21/Adressebok3DAA_21/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adressebok3DAA_21
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] personer = new string[5];
+        int antPersoner = 0;
+
+        private void btLagre_Click(object sender, EventArgs e)
+        {
+            if (antPersoner <= 4)
+            {
+                personer[antPersoner] = tbNavn.Text + ", " + tbTlf.Text;
+                antPersoner++;
+            }
+        }
+    }
+}
